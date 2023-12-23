56871b12f89265374a521323e866960f6f3fb040
Mateus Hamade <mateus.hamade@hotmail.com>
2023-12-23 10:28:49 -0300

    Fix indentation and linting issues

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import "./Main.css";
 
 const Main = () => {
-    return (
-        <div>
-            <h1>Main</h1>
-        </div>
-    );
+   return (
+      <div>
+         <h1>Main</h1>
+      </div>
+   );
 };
 
 export default Main;
