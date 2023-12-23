575a2ca2e19eeb7c1e6e5419645ddf9ce27c2141
Mateus Hamade <mateus.hamade@hotmail.com>
2023-12-23 17:00:48 -0300

    Refactor Home component and update styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import Header from "../components/header/Header";
+import Main from "../components/main/Main";
+import Footer from "../components/footer/Footer";
+
 import "./Home.css";
 
 const Home = () => {
    return (
-      <div>
-         <h1>Home</h1>
+      <div className="container">
+         <Header />
+         <Main />
+         <Footer />
       </div>
    );
 };
