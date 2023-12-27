5846bcabaed96cf0c0d012f1367f545f4daa2a67
Mateus Hamade <mateus.hamade@aluno.ufop.edu.br>
2023-12-26 21:29:31 -0300

    Update Main.jsx and related CSS files

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -18,6 +18,42 @@ const Main = () => {
                <strong>Bebida:</strong> Suco de laranja. <br />
                <strong>Valor:</strong> R$ 7,05. </p>
          </div>
+         <div className="user-container">
+               <User2 className="icon"/>
+               <p className="user-message">Olá, você poderia me dizer qual o cardápio do R.U de hoje?</p>
+         </div>
+         <div className="bot-container">
+               <Bot className="icon"/>
+               <p className="bot-message">Claro, o cardápio do R.U hoje está bem gostoso! <br />
+               <strong>Prato principal:</strong> Arroz, Feijão, Salada de alface com tomate, angu, bife de porco ao molho. <br />
+               <strong>Sobremesa:</strong> Salada de fruta. <br />
+               <strong>Bebida:</strong> Suco de laranja. <br />
+               <strong>Valor:</strong> R$ 7,05. </p>
+         </div>
+         <div className="user-container">
+               <User2 className="icon"/>
+               <p className="user-message">Olá, você poderia me dizer qual o cardápio do R.U de hoje?</p>
+         </div>
+         <div className="bot-container">
+               <Bot className="icon"/>
+               <p className="bot-message">Claro, o cardápio do R.U hoje está bem gostoso! <br />
+               <strong>Prato principal:</strong> Arroz, Feijão, Salada de alface com tomate, angu, bife de porco ao molho. <br />
+               <strong>Sobremesa:</strong> Salada de fruta. <br />
+               <strong>Bebida:</strong> Suco de laranja. <br />
+               <strong>Valor:</strong> R$ 7,05. </p>
+         </div>
+         <div className="user-container">
+               <User2 className="icon"/>
+               <p className="user-message">Olá, você poderia me dizer qual o cardápio do R.U de hoje?</p>
+         </div>
+         <div className="bot-container">
+               <Bot className="icon"/>
+               <p className="bot-message">Claro, o cardápio do R.U hoje está bem gostoso! <br />
+               <strong>Prato principal:</strong> Arroz, Feijão, Salada de alface com tomate, angu, bife de porco ao molho. <br />
+               <strong>Sobremesa:</strong> Salada de fruta. <br />
+               <strong>Bebida:</strong> Suco de laranja. <br />
+               <strong>Valor:</strong> R$ 7,05. </p>
+         </div>
       </main>
    );
 };
