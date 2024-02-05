43f02899097ecfc2fe820190cd30dba45bf86da7
Mateus Hamade <mateus.hamade@aluno.ufop.edu.br>
2024-02-05 10:40:34 -0300

    Adds OpenAI dependency and updates API service

--- src/services/api_service.js
+++ src/services/api_service.js
@@ -1,13 +1,22 @@
-import axios from "axios";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+
+const config = new Configuration({
+  apiKey: "sk-<YOUR_API>",
+});
+
+const openai = new OpenAIApi(config);
 
 const sendMessage = async (message) => {
     try {
-        const response = await axios.post("http://localhost:3001/chat", { message });
-        return response.data.message;
-        
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            temperature: 0.7,
+            messages: [{ role: "user", content: message }],
+        });
+        return response.data.choices[0].message.content;
     } catch (error) {
-        return error.response.data.message;
+        return error;
     }
-}
+};
 
 export { sendMessage };
